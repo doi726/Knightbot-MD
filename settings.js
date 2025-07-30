d0a7539305bed6c3d35407e96d7c252917de8506
doi726 <daffaoktavi726@gmail.com>
2025-07-31 00:37:52 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: '6283171860048',
-  author: '6289620611161‎',
-  botName: "6283171860048",
-  botOwner: '6289620611161', // Your name
-  ownerNumber: '6283171860048', //Set your number here without + symbol, just add country code & number without any space
+  packname: '6283161113153',
+  author: '6283161113153',
+  botName: "6283161113153",
+  botOwner: '6283161113153', // Your name
+  ownerNumber: '6283161113153', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
